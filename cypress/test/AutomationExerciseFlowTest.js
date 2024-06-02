c0726eb0dc490b80e240997eb2370207e4182dab
Hoa Ning <git config --global user.email>
2024-06-03 00:29:51 +0700

    update log

--- cypress/test/AutomationExerciseFlowTest.js
+++ cypress/test/AutomationExerciseFlowTest.js
@@ -102,6 +102,7 @@ describe('Automation Exercise Flow Test Cases', () => {
   })
 
   it('Test Case 6: Contact Us Form', () => {
+    console.log('test')
     homepageComponents.getBanner().should('be.visible')
     headerComponents.getContactUs().click()
     contactUsPage.getTitleGetInTouch().should('be.visible')
