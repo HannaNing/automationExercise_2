960b59b7b2330f960290b23372d9359405cdb26b
Hoa Ning <git config --global user.email>
2024-06-03 00:35:16 +0700

    update va xoa file

--- cypress/test/AutomationExerciseFlowTest.js
+++ cypress/test/AutomationExerciseFlowTest.js
@@ -102,7 +102,6 @@ describe('Automation Exercise Flow Test Cases', () => {
   })
 
   it('Test Case 6: Contact Us Form', () => {
-    console.log('test')
     homepageComponents.getBanner().should('be.visible')
     headerComponents.getContactUs().click()
     contactUsPage.getTitleGetInTouch().should('be.visible')
